9e52191a18b3492a8a9cce685a2b1f15970d0cf1
sapphire- <sooyeon.song22@gmail.com>
2014-08-22 07:15:27 +0900

    ㄲ아아아아아아ㅏ아
    드뎓ㄷㄷ뎓뎓

--- apps/models.py
+++ apps/models.py
@@ -13,31 +13,31 @@ class member_info(db.Model):
 class moim(db.Model):
     moim_id = db.Column(db.Integer, primary_key=True)
     moim_name = db.Column(db.String(255))
-    member_id = db.Column(db.Integer, db.ForeignKey('member_info.id'), primary_key=True)
+    member_id = db.Column(db.Integer, db.ForeignKey('member_info.member_id'), primary_key=True, autoincrement=False)
     member_info = db.relationship('member_info', backref=db.backref('moim', cascade='all, delete-orphan', lazy='dynamic'))
     #member_id = member_info.member_id
 
 class moim_join(db.Model):
-    moim_id = db.Column(db.Integer, db.ForeignKey('moim.id'), primary_key=True)
+    moim_id = db.Column(db.Integer, db.ForeignKey('moim.moim_id'), primary_key=True, autoincrement=False)
     moim = db.relationship('moim', backref=db.backref('moim_join', cascade='all, delete-orphan', lazy='dynamic'))
     # moim_id = moim.moim_id
-    member_id = db.Column(db.Integer, db.ForeignKey('member_info.id'), primary_key=True)
+    member_id = db.Column(db.Integer, db.ForeignKey('member_info.member_id'), primary_key=True, autoincrement=False)
     member_info = db.relationship('member_info', backref=db.backref('moim_join', cascade='all, delete-orphan', lazy='dynamic'))
     #member_id = member_info.member_id
     member_name = db.Column(db.String(255))
     invited = db.Column(db.Integer)
 
 class moim_pay(db.Model):
-    moim_id = db.Column(db.Integer, db.ForeignKey('moim.id'), primary_key=True)
+    moim_id = db.Column(db.Integer, db.ForeignKey('moim.moim_id'), primary_key=True, autoincrement=False)
     moim = db.relationship('moim', backref=db.backref('moim_pay', cascade='all, delete-orphan', lazy='dynamic'))
     # moim_id = moim.moim_id
-    number = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.Integer, primary_key=True, autoincrement=False)
     content = db.Column(db.String(255))
     payment_all = db.Column(db.Integer)
     moim_date = db.Column(db.DateTime(), default=db.func.now())
 
 class moim_mem(db.Model):
-    moim_id = db.Column(db.Integer, db.ForeignKey('moim.id'), primary_key=True)
+    moim_id = db.Column(db.Integer, db.ForeignKey('moim.moim_id'), primary_key=True, autoincrement=False)
     moim = db.relationship('moim', backref=db.backref('moim_mem', cascade='all, delete-orphan', lazy='dynamic'))
-    member_id = db.Column(db.Integer, db.ForeignKey('member_info.id'), primary_key=True)
+    member_id = db.Column(db.Integer, db.ForeignKey('member_info.member_id'), primary_key=True, autoincrement=False)
     member_info = db.relationship('member_info', backref=db.backref('moim_mem', cascade='all, delete-orphan', lazy='dynamic'))
